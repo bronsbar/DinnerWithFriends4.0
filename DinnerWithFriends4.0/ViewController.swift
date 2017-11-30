40f678d7cd960f4968d42d501fd3b05bd99d6c16
Bart Bronselaer <bart.bronselaer@royalparkinvestments.com>
2017-11-30 11:08:09 +0100

    Initial Commit

---
+++ DinnerWithFriends4.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DinnerWithFriends4.0
+//
+//  Created by Bart Bronselaer on 30/11/17.
+//  Copyright © 2017 Bart Bronselaer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
